2b34f4c751c41a067798e39e275685c0c189b64d
Ali <ali.abdalla@outlook.fr>
2023-01-31 18:21:21 +0100

    display the list of the series

--- src/reducer/reducerTV.jsx
+++ src/reducer/reducerTV.jsx
@@ -3,11 +3,10 @@ let DATA = [];
 const initialState = {
     data: DATA
 };
-
-export const reducerSeries = (state = initialState, action) => {
+export const reducerTv = (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_DATA': 
-            return { ...state, data: action.data }
+        return { ...state, data: action.data }
         default:
             return state;
     }
