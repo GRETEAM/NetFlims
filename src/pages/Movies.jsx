d77991cc85cf7f9e5b601bb223be3e3e4369fae2
Ali <ali.abdalla@outlook.fr>
2023-01-31 19:15:51 +0100

    add the date of the movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -18,12 +18,18 @@ const Movies = () => {
     useEffect(() => {
         SetData(movies.data)
     }, [movies]);
+    console.log(data)
     return (
         //style a mettre plus tard dans le style
         <div style={{position:"relative"}}>
             {data.length == 0 || undefined ? <Loader/>:
             data.map((movie)=>{
-                return <p key={movie.id}> {movie.original_title || movie.name}</p>
+                if(movie.media_type === "movie")
+                return <div key={movie.id}> 
+                    <p>{movie.original_title || movie.name}</p>
+                    <p>{movie.release_date}</p>
+                    
+                </div>
             })
 
             }
