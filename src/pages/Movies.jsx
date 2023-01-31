38cd4bb6432a3d25c401b6240df2174fa60871e2
Ali <ali.abdalla@outlook.fr>
2023-01-31 21:28:14 +0100

    add the same scss trending for movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,10 @@
+import moment from "moment";
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../actions/actionMovies';
+import { Swiper, SwiperSlide } from "swiper/react";
 import Loader from '../components/Loader';
+import "swiper/scss";
 
 const Movies = () => {
     const [data, SetData] = useState([])
@@ -14,27 +17,42 @@ const Movies = () => {
         loadMovies();
     }, [dispatch]);
     const movies = useSelector(store => store.reducerMovies);
-    // console.log(movies)
     useEffect(() => {
         SetData(movies.data)
     }, [movies]);
-    console.log(data)
     return (
         //style a mettre plus tard dans le style
-        <div style={{position:"relative"}}>
-            {data.length == 0 || undefined ? <Loader/>:
-            data.map((movie)=>{
-                if(movie.media_type === "movie")
-                return <div key={movie.id}> 
-                    <p>{movie.original_title || movie.name}</p>
-                    <p>{movie.release_date}</p>
-                    
-                </div>
-            })
+        // <div style={{ position: "relative" }}>
+        <main className="container">
+            <section className="trending">
+                <h1 className="title">Movies</h1>
+            </section>
+            {data.length == 0 || undefined ? <Loader /> :
+                <Swiper className="slider">
+                    {data.map((movie) => {
+                        if (movie.media_type === "movie")
+                            return <SwiperSlide className="slider-content" key={movie.id}>
+                                <img
+                                    className="slider-content-img"
+                                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                                    alt={movie.title || movie.name}
+                                />
+                                <div className="slider-content-text">
+                                    <div className="infos trending-title-infos">
+                                        <p>{moment(movie.release_date).format("YYYY")}</p>
+                                        <span className="infos-circle"></span>
+                                        <p className="infos-movie trending-title-infos">{movie.media_type}</p>
+                                    </div>
+                                    <h2 className="trending-title">
+                                        {movie.original_title || movie.name}
+                                    </h2>
+                                </div>
+                            </SwiperSlide>
 
+                    })}
+                </Swiper>
             }
-          
-        </div>
+        </main>
     )
 }
 export default Movies;
