6a68b72a6133b15b1b91d8539abfe5dcdcf1c7f7
Ali <ali.abdalla@outlook.fr>
2023-01-31 17:18:16 +0100

    display the series

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from './pages/Dashboard';
 import Movies from "./pages/Movies";
+import Series from "./pages/Series";
 import store from "./Store";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Dashboard />} />
           <Route path='/movies' element={<Movies />} /> 
+          <Route path='/series' element={<Series />} /> 
         </Routes>
       </BrowserRouter>
     </Provider>
