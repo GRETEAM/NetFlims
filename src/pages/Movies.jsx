573357aa00b3cd44e21e847d70c3d64f032d9dd1
Ali <ali.abdalla@outlook.fr>
2023-01-31 17:05:15 +0100

    log all the movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../actions/actionMovies';
 
 const Movies = () => {
-   const dispatch = useDispatch();
+    const [data, SetData] = useState([])
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const loadMovies = async () => {
@@ -11,11 +12,22 @@ const Movies = () => {
         };
         loadMovies();
     }, [dispatch]);
-
     const movies = useSelector(store => store.reducerMovies);
-    console.log(movies);
+    // console.log(movies)
+    useEffect(() => {
+        SetData(movies.data)
+    }, [movies]);
     return (
-        <div>Movies</div>
+        <div>
+            movies
+            {data.length == 0 || undefined ? console.log("loading"):
+            data.map((movie)=>{
+                    console.log(movie);
+                })
+
+            }
+          
+        </div>
     )
 }
 export default Movies;
