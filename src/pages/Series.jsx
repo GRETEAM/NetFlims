1bc2a6d9336eab144bfeac1f00ef1bdb02202566
Ali <ali.abdalla@outlook.fr>
2023-02-01 12:16:15 +0100

    add css similare for series

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,7 +1,11 @@
-import { useEffect, useState } from 'react';
+import moment from "moment";
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../actions/actionMovies';
+import { Swiper, SwiperSlide } from "swiper/react";
 import Loader from '../components/Loader';
+import "swiper/scss";
+
 
 const Series = () => {
     const [data, SetData] = useState([])
@@ -20,16 +24,35 @@ const Series = () => {
     }, [Series]);
     return (
         //style a mettre plus tard dans le style
-        <div style={{position:"relative"}}>
-            {data.length == 0 || undefined ? <Loader/>:
-            data.map((serie)=>{
-                if(serie.media_type === "tv")
-                return <p key={serie.id}> {serie.original_title || serie.name}</p>
-            })
-
+        <main className="container">
+            <section className="trending">
+                <h1 className="title">Series</h1>
+            </section>
+            {data.length == 0 || undefined ? <Loader /> :
+                <Swiper className="slider">
+                    {data.map((serie) => {
+                        if (serie.media_type === "tv")
+                            return <SwiperSlide className="slider-content" key={serie.id}>
+                                <img
+                                    className="slider-content-img"
+                                    src={`https://image.tmdb.org/t/p/w500${serie.backdrop_path}`}
+                                    alt={serie.title || serie.name}
+                                />
+                                <div className="slider-content-text">
+                                    <div className="infos trending-title-infos">
+                                        <p>{moment(serie.release_date).format("YYYY")}</p>
+                                        <span className="infos-circle"></span>
+                                        <p className="infos-serie trending-title-infos">{serie.media_type}</p>
+                                    </div>
+                                    <h2 className="trending-title">
+                                        {serie.original_title || serie.name}
+                                    </h2>
+                                </div>
+                            </SwiperSlide>
+                    })}
+                </Swiper>
             }
-          
-        </div>
+        </main>
     )
 }
 export default Series;
