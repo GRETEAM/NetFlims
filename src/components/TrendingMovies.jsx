f1007b9df395e5a700c0c8be04b94c68877e0ce2
blazrr <selmhamedi@gmail.com>
2023-02-06 17:51:11 +0100

    Bookmarks on all cards

--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -5,14 +5,14 @@ import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/scss";
 import Loader from "./Loader";
+import Pin from "./Pin";
 
 const TrendingMovies = ({ movies }) => {
-
   return (
     <section className="recommend">
       {movies.length === 0 || undefined ? (
-          <Loader />
-        ) : (
+        <Loader />
+      ) : (
         <Swiper
           rewind={true}
           className="slider"
@@ -23,25 +23,28 @@ const TrendingMovies = ({ movies }) => {
           {movies.map((movie) => {
             return (
               <SwiperSlide className="slider-content" key={movie.id}>
-                <Link to={`/${movie.id}`} state={{ movie: movie }}>
-                  <img
-                    className="slider-content-img"
-                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                    alt={movie.title || movie.name}
-                  />
-                  <div className="slider-content-text">
-                    <div className="infos trending-title-infos">
-                      <p>{moment(movie.release_date).format("YYYY")}</p>
-                      <span className="infos-circle"></span>
-                      <p className="infos-movie trending-title-infos">
-                        {movie.media_type}
-                      </p>
+                <div className="card">
+                  <Link to={`/${movie.id}`} state={{ movie: movie }}>
+                    <img
+                      className="slider-content-img"
+                      src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                      alt={movie.title || movie.name}
+                    />
+                    <div className="slider-content-text">
+                      <div className="infos trending-title-infos">
+                        <p>{moment(movie.release_date).format("YYYY")}</p>
+                        <span className="infos-circle"></span>
+                        <p className="infos-movie trending-title-infos">
+                          {movie.media_type}
+                        </p>
+                      </div>
+                      <h2 className="trending-title">
+                        {movie.original_title || movie.name}
+                      </h2>
                     </div>
-                    <h2 className="trending-title">
-                      {movie.original_title || movie.name}
-                    </h2>
-                  </div>
-                </Link>
+                  </Link>
+                  <Pin pageData={movie} />
+                </div>
               </SwiperSlide>
             );
           })}
--- src/pages/Bookmark.jsx
+++ src/pages/Bookmark.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import useFetch from "../hooks/useFetch";
 import moment from "moment";
 import { Link } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/scss";
-import Loader from "../components/Loader";
+import { Navigation } from "swiper";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Pin from "../components/Pin";
 
 const Bookmark = () => {
   const user = useSelector((store) => store.reducerUser);
@@ -19,29 +20,38 @@ const Bookmark = () => {
   return (
     <div className="container">
       <section className="recommend">
-        <Swiper className="slider">
+        <Swiper
+          rewind={true}
+          className="slider"
+          slidesPerView={"auto"}
+          navigation={true}
+          modules={[Navigation]}
+        >
           {bookmarks?.map((movie) => {
             return (
               <SwiperSlide className="slider-content" key={movie.id}>
-                <Link to={`/${movie.id}`} state={{ movie: movie }}>
-                  <img
-                    className="slider-content-img"
-                    src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                    alt={movie.title || movie.name}
-                  />
-                  <div className="slider-content-text">
-                    <div className="infos trending-title-infos">
-                      <p>{moment(movie.release_date).format("YYYY")}</p>
-                      <span className="infos-circle"></span>
-                      <p className="infos-movie trending-title-infos">
-                        {movie.media_type}
-                      </p>
+                <div className="card">
+                  <Link to={`/${movie.id}`} state={{ movie: movie }}>
+                    <img
+                      className="slider-content-img"
+                      src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                      alt={movie.title || movie.name}
+                    />
+                    <div className="slider-content-text">
+                      <div className="infos trending-title-infos">
+                        <p>{moment(movie.release_date).format("YYYY")}</p>
+                        <span className="infos-circle"></span>
+                        <p className="infos-movie trending-title-infos">
+                          {movie.media_type}
+                        </p>
+                      </div>
+                      <h2 className="trending-title">
+                        {movie.original_title || movie.name}
+                      </h2>
                     </div>
-                    <h2 className="trending-title">
-                      {movie.original_title || movie.name}
-                    </h2>
-                  </div>
-                </Link>
+                  </Link>
+                  <Pin pageData={movie} />
+                </div>
               </SwiperSlide>
             );
           })}
