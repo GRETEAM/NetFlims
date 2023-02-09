d7df221f4b267ff2e16adc44268fe34f7a6e5397
Alexandre-st <alexandre.saintprix@gmail.com>
2023-02-09 17:05:10 +0100

    Add the rest of works

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { useLocation, useParams } from "react-router-dom";
 import NotAvailble from "../assets/no-image-available-icon-6.png";
 import Pin from "../components/Pin";
@@ -6,21 +7,16 @@ import Similar from "../components/Similar";
 const Details = () => {
   const location = useLocation();
   const data = location.state.movie;
-
+  
   return (
     <main className="container">
       <div className="detail_container ">
         <div className="detail-image">
           <Pin pageData={data} />
           {data.backdrop_path === null ? (
-            <img
-              
-              src={NotAvailble}
-              alt={data.name || data.original_title}
-            />
+            <img src={NotAvailble} alt={data.name || data.original_title} />
           ) : (
             <img
-              
               src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`}
               alt={data.name || data.original_title}
             />
@@ -30,8 +26,10 @@ const Details = () => {
         <div className="detail-card">
           <h1>{data.name || data.original_title}</h1>
           <p className="overview">{data.overview}</p>
-          <p className="vote">Vote: {data.vote_average}/10</p>
-          <p className="revenue">Revenue : ${data.revenue}</p>
+          <div className="detail-card-info">
+            <p>Release Date : {moment(data.release_date).format("MMMM Do YYYY")}</p>
+            <p className="vote">Vote: {data.vote_average} / 10</p>
+          </div>
         </div>
       </div>
       <Similar media_type={data.media_type} movie_id={data.id} />
